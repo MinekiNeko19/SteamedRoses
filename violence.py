ede292850061a87bc8d39400eff627815b855537
Ruoxin Cai <2004cai.r@gmail.com>
2023-02-04 13:33:08 -0500

    tkinter screen

--- violence.py
+++ violence.py
@@ -1,10 +1,9 @@
-import tkinter, numpy, random, math
+import tkinter as tk
+import numpy, random, math
                   # width of one site in pixels (change if desired)
 canvasWidth = 1536
 canvasHeight = 864# full width of canvas in pixels
 
-running = False                     # will be true when simulation is running
-theWindow = tkinter.Tk()            # create the GUI window
-theWindow.title("player screen")
-
-theCanvas = tkinter.Canvas(theWindow, width=canvasWidth, height=canvasHeight)
+window=tk.Tk()
+window.geometry("1536x864")
+window.mainloop()
