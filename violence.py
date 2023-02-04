d3ff9d938271d616ad9ff0e3d2c627bce685edba
Ruoxin Cai <2004cai.r@gmail.com>
2023-02-04 13:47:06 -0500

    canvassing

--- violence.py
+++ violence.py
@@ -1,9 +1,13 @@
 import tkinter as tk
+from tkinter import *
 import numpy, random, math
                   # width of one site in pixels (change if desired)
 canvasWidth = 1536
 canvasHeight = 864# full width of canvas in pixels
 
-window=tk.Tk()
-window.geometry("1536x864")
-window.mainloop()
+win=tk.Tk()
+win.geometry("1600x800")
+p1=Canvas(win, width=800, height=800, bg="#FF5733")
+p1.pack(side="left")
+#p2=Canvas(win, width=800, height=500)
+win.mainloop()
